56e95ef9f01a49bde687aa9e6d0d345d52a5e866
Durga Prasad <Durga.Prasad@Vitality.co.uk>
2020-02-06 18:59:20 +0530

    Redux Implemented

--- src/App.js
+++ src/App.js
@@ -2,16 +2,32 @@ import React from "react";
 import "./App.css";
 import SignIn from "./Login";
 import ListDetails from "./ListDetails";
+import { connect } from "react-redux";
+import { simpleAction } from "./actions/SimpleAction";
+
 import { Route, BrowserRouter as Router } from "react-router-dom";
-function App() {
+
+const mapStateToProps = state => ({
+  ...state
+});
+
+const mapDispatchToProps = dispatch => ({
+  simpleAction: () => dispatch(simpleAction())
+});
+function App(props) {
+  function simpleAction(event) {
+    props.simpleAction();
+  }
   return (
     <div>
       <Router>
         <Route path="/" exact component={SignIn} />
         <Route path="/ListDetails" exact component={ListDetails} />
       </Router>
+      <button onClick={simpleAction}>Test redux action</button>
+      <pre>{JSON.stringify(props)}</pre>
     </div>
   );
 }
 
-export default App;
+export default connect(mapStateToProps, mapDispatchToProps)(App);
